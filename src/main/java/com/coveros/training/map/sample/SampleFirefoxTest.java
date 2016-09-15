d3afd396b4dadf6906b4c224da828f8c1256019a
brian <brian.hicks@coveros.com>
2016-09-14 20:04:11 -0400

    fix SampleFirefoxTest by including geckodriver configuration and using MarionetteDriver class

--- src/main/java/com/coveros/training/map/sample/SampleFirefoxTest.java
+++ src/main/java/com/coveros/training/map/sample/SampleFirefoxTest.java
@@ -7,7 +7,10 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.MarionetteDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.github.bonigarcia.wdm.MarionetteDriverManager;
 
 /**
  * Simple test class that verifies proper installation of MTW tools and
@@ -19,9 +22,21 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class SampleFirefoxTest {
 	private WebDriver driver;
 
+	static {
+		MarionetteDriverManager.getInstance().setup();
+		// if you didn't update the Path system variable to add the full
+		// directory path to the executable as above mentioned then doing this
+		// directly through code
+
+		System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver/geckodriver.exe");
+	}
+
 	@Before
 	public void setUp() throws Exception {
-		driver = new FirefoxDriver();
+		// Now you can Initialize marionette driver to launch firefox
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setCapability("marionette", true);
+		driver = new MarionetteDriver(capabilities);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
@@ -31,7 +46,7 @@ public class SampleFirefoxTest {
 	 * @throws Exception
 	 */
 	@Test
-	public void amazon() throws Exception {
+	public void coveros() throws Exception {
 		driver.get("https://www.coveros.com/");
 		assertEquals("Coveros", driver.getTitle());
 	}
